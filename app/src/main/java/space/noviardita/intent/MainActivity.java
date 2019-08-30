de4a8eadfe1be3845edb54b2d08f0e15b909a877
Noviardita Ramadhani <42690529+Nvdta26@users.noreply.github.com>
2019-08-30 08:46:52 +0700

    Intent
    perhitungan bangun datar

---
+++ app/src/main/java/space/noviardita/intent/MainActivity.java
@@ -0,0 +1,42 @@
+package space.noviardita.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnpanjang = findViewById(R.id.btnpersegipanjang);
+        btnpanjang.setOnClickListener(this);
+
+        Button btnlingkaran = findViewById(R.id.btnlingkaran);
+        btnlingkaran.setOnClickListener(this);
+
+        Button btnsegitiga = findViewById(R.id.btnsegitiga);
+        btnsegitiga.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnpersegipanjang:
+                Intent moveIntent = new Intent(MainActivity.this, HitungPesergiPanjang.class);
+                startActivity(moveIntent);
+                break;
+            case R.id.btnlingkaran:
+                Intent moveIntentt = new Intent(MainActivity.this, Lingkaran.class);
+                startActivity(moveIntentt);
+                break;
+            case R.id.btnsegitiga:
+                Intent moveIntenttt = new Intent(MainActivity.this, HitungSegitiga.class);
+                startActivity(moveIntenttt);
+                break;
+        }
+    }
+}
